8e300e123a93c87a517576e26aebe4ee367fe3ac
Nemanja Milkov <n.milkov@levi9.com>
2023-08-18 12:34:26 +0200

    Added location updates instead of location request on demand

--- app/src/main/java/com/levi9/lbnnative/MainActivity.kt
+++ app/src/main/java/com/levi9/lbnnative/MainActivity.kt
@@ -8,9 +8,9 @@ import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Build
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
@@ -20,16 +20,17 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.Granularity
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
-import com.google.android.gms.tasks.CancellationToken
-import com.google.android.gms.tasks.CancellationTokenSource
-import com.google.android.gms.tasks.OnTokenCanceledListener
 import com.levi9.lbnnative.databinding.ActivityMainBinding
 import com.levi9.lbnnative.receiver.GeofenceBroadcastReceiver
 import com.levi9.lbnnative.receiver.GeofenceBroadcastReceiver.Companion.NOTIFICATION_CHANNEL_DESCRIPTION
 import com.levi9.lbnnative.receiver.GeofenceBroadcastReceiver.Companion.NOTIFICATION_CHANNEL_ID
-import java.util.UUID
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -44,6 +45,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var fusedLocationClient: FusedLocationProviderClient
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var locationRequest: LocationRequest
+
+    private lateinit var locationCallback: LocationCallback
 
     private val requestFineLocationPermissionLauncher =
         registerForActivityResult(
@@ -95,6 +100,26 @@ class MainActivity : AppCompatActivity() {
         pendingIntent = getGeofencePendingIntent()
 
         handleNotificationsPermission()
+
+        handleLocationUpdates()
+    }
+
+    private fun handleLocationUpdates() {
+        locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000).apply {
+            setMinUpdateDistanceMeters(10F)
+            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult) {
+                for (location in locationResult.locations) {
+                    val lat = location.latitude.toString()
+                    val lon = location.longitude.toString()
+                    Toast.makeText(applicationContext, "$lat - $lon", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -102,7 +127,6 @@ class MainActivity : AppCompatActivity() {
         geofencingClient.addGeofences(geofencingRequest, pendingIntent).run {
             addOnSuccessListener {
                 Log.e("===", "Geofence list registered successfully")
-                setElementClickListener()
             }
             addOnFailureListener {
                 Log.e("===", "Failed to add geofence list")
@@ -113,7 +137,7 @@ class MainActivity : AppCompatActivity() {
     private fun fillGeofenceList() {
         geofenceList.add(
             Geofence.Builder()
-                .setRequestId(UUID.randomUUID().toString())
+                .setRequestId("SOME_ID_1")
                 .setCircularRegion(
                     45.2569469,
                     19.844466,
@@ -193,29 +217,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("MissingPermission")
-    private fun setElementClickListener() {
-        binding.tvGetLocation.setOnClickListener {
-            fusedLocationClient.getCurrentLocation(
-                Priority.PRIORITY_HIGH_ACCURACY,
-                object : CancellationToken() {
-                    override fun onCanceledRequested(p0: OnTokenCanceledListener) =
-                        CancellationTokenSource().token
+    override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+    }
 
-                    override fun isCancellationRequested() = false
-                })
-                .addOnSuccessListener { location: Location? ->
-                    if (location == null)
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Cannot get location.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    else {
-                        Toast.makeText(this@MainActivity, "Fetched location.", Toast.LENGTH_SHORT)
-                            .show()
-                    }
-                }
+    private fun startLocationUpdates() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
         }
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    override fun onPause() {
+        super.onPause()
+        stopLocationUpdates()
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
     }
 }
